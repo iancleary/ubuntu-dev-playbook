32955a4fcacb0c47b8811055f224eb58d0ef5038
iancleary <iancleary@pm.me>
2020-02-26 19:45:44 -0700

    update bootstrap script to autoremove, user install ansible

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,19 @@
 
 echo "Installing make and ansible"
 
+# Remove any version of ansible
+sudo apt remove -y ansible
+
+## Remove old packages
+sudo apt -y autoremove
+
 # Make for ease of use, python-apt for --check ansible flag
 sudo apt-get install -y make python3-apt
 
 ## Install latest Ansible (snap is only in Ansible >= 2.8)
 sudo apt update
 sudo apt install -y software-properties-common
-python3 -m pip install ansible
+python3 -m pip install --user ansible
 ansible --version | grep "python version"
 
 ## The below may work when Ubuntu fully deprecates python2
