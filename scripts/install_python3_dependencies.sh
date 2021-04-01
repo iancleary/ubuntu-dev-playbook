fb24328c5d69b8ace434fb919f09fe3531b9f0a4
iancleary <iancleary@hey.com>
2021-03-31 22:24:41 -0700

    Added testresources to scripts/install_python3_dependencies.sh

--- scripts/install_python3_dependencies.sh
+++ scripts/install_python3_dependencies.sh
@@ -8,6 +8,7 @@ sudo chmod -R 755 "$FOLDER"
 
 python3 -m pip install --user --upgrade pip
 python3 -m pip install --upgrade keyrings.alt --user
+python3 -m pip install --user testresources
 python3 -m pip install --user --upgrade setuptools
 python3 -m pip install --user wheel
 python3 -m pip install --user -r requirements_ansible.txt
