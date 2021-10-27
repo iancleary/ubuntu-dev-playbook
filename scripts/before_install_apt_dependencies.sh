35d7aaa2773bf110d74ffd550e60d80062aeedef
icancclearynow <hello@iancleary.me>
2021-10-26 21:51:25 -0700

    Added tasks, faster scripts, added extra-packages, saner ansible.cfg

--- scripts/before_install_apt_dependencies.sh
+++ scripts/before_install_apt_dependencies.sh
@@ -4,9 +4,7 @@ sudo apt -y autoremove
 sudo apt update
 
 # Python3 & Pip3
-sudo apt install -y software-properties-common
-sudo apt install -y python3-setuptools python3-apt python3-pip
+sudo apt install -y software-properties-common python3-setuptools python3-apt python3-pip
 
 # Linting
-sudo apt install -y yamllint
-sudo apt install -y shellcheck
+sudo apt install -y yamllint shellcheck
--- scripts/install_python3_dependencies.sh
+++ scripts/install_python3_dependencies.sh
@@ -6,10 +6,6 @@ mkdir -p "$FOLDER"
 sudo chown -R "$USER:$USER" "$FOLDER"
 sudo chmod -R 755 "$FOLDER"
 
-python3 -m pip install --user --upgrade pip
-python3 -m pip install --upgrade keyrings.alt --user
-python3 -m pip install --user testresources
-python3 -m pip install --user --upgrade setuptools
-python3 -m pip install --user wheel
-python3 -m pip install --user linode_api4
+python3 -m pip install --user --upgrade pip setuptools keyrings.alt
+python3 -m pip install --user testresources wheel linode_api4
 python3 -m pip install --user -r requirements_ansible.txt
