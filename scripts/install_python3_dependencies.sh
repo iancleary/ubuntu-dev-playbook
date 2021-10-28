6b577a68deeacc71d51714a13263e624b8810c55
icancclearynow <hello@iancleary.me>
2021-10-28 10:01:09 -0700

    Updated scripts

--- scripts/install_python3_dependencies.sh
+++ scripts/install_python3_dependencies.sh
@@ -7,5 +7,5 @@ sudo chown -R "$USER:$USER" "$FOLDER"
 sudo chmod -R 755 "$FOLDER"
 
 python3 -m pip install --user --upgrade pip setuptools keyrings.alt
-python3 -m pip install --user testresources wheel linode_api4
+python3 -m pip install --user testresources wheel
 python3 -m pip install --user -r requirements_ansible.txt
