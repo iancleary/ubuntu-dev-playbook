e2f3237ae655d1420ed8fb1c34e1453e7bf02ab6
icancclearynow <hello@iancleary.me>
2021-10-28 12:20:23 -0700

    Remove redundant apt packages for python from scripts

--- scripts/before_install_apt_dependencies.sh
+++ scripts/before_install_apt_dependencies.sh
@@ -4,7 +4,7 @@ sudo apt -y autoremove
 sudo apt update
 
 # Python3 & Pip3
-sudo apt install -y software-properties-common python3-setuptools python3-apt python3-pip
+sudo apt install -y python3-pip
 
 # Linting
 sudo apt install -y yamllint shellcheck
