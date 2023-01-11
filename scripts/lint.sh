1512f5fff6188f700fb64934f340ca3b2ec07aa8
iancleary <hello@iancleary.me>
2023-01-10 20:46:29 -0700

    Remove markdown linting

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,8 +6,3 @@ yamllint .
 # Bash
 shellcheck ./*.sh
 shellcheck scripts/*.sh
-
-# Markdown
-# Ignore CHANGELOG.md as it has duplicate headings by design
-markdownlint README.md AUTHORS.md
-markdownlint docs/*.md
